441c3ffa596d46796615acf5274414283d224676
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-03-08 00:06:03 -0800

    Update p3codeBase.s

--- p3codeBase.s
+++ p3codeBase.s
@@ -1,4 +1,4 @@
-   .data
+ .data
         endl:    .asciiz  "\n"   # used for cout << endl;
         albl:    .asciiz  "Value of a: " # label for a
         blbl:    .asciiz  "Value of b: " # label for b
@@ -12,18 +12,20 @@
 # d --> $s3
 
 main:    
-addi       $s0, 5              # load value 5 to a
-addi       $s1, 6              # load value 6 to b
-addi       $s2, 7              # load value 7 to c
-addi       $s3, -1             # load value -1 to d
-addi       $t0, 10             # load value 10 to $t0 for next compare function
+addi       $s0, 5              # add value 5 to a as initial
+addi       $s1, 6              # add value 6 to b as initial
+addi       $s2, 7              # add value 7 to c as initial
+addi       $s3, -1             # add value -1 to d as initial
+addi       $t0, 10             # add value 10 to $t0 for next compare function
 
-slt      $t1, $s0, $t0       # Set on $s0 less than $t0   a<10
-beq      $t1, 0, Else1       # Branch on equal 0, means if the statement is false, a>=10, then run Else2
-add      $s0, $s0, 1         # If a<10 is true, then a = a +1
-j        End1              # Jump to End funtion
-Else1 :
-         addi   $s0, $s0, -1         # a = a-1
+blt      $s0, $t0, Lable1
+bgt      $s0, $t0, Lable2       # Set on $s0 less than $t0   a<10
+Lable1: 
+         add  $s0, $s0, 1         # If a<10 is true, then a = a +1
+j        End1                     # Jump to End funtion
+Lable2 :
+         addi $s0, $s0, -1         # a = a-1
+j        End1                     # Jump to End funtion
 End1 :                             # End if function
 
 add      $s3,$s2, $s0                # d = c+a
@@ -33,11 +35,11 @@ slt      $t1, $s1, $t0               # Set on $s1 less than $t0   b<10
 beq      $t1, 0, Else2               # Branch on equal 0, means if the statement is false, b>=10, then run Else2
 add      $s1, $s1, 1                 # If b<10 is true, then a = a+1
 addi     $s2, $s2, -1                # If b<10 is true, then c = c-1
-j        End2                      # Jump to End funtion
+j        End2                        # Jump to End funtion
 Else2 :
          add    $s2, $s2,  1         # c = c+1
          addi   $s1, $s1, -1         # b = b-1
-End2 :                             # End if function
+End2 :                               # End if function
 
 add      $s0, $s1, $s2               # a=b+c
 add      $s1, $s3, $s2               # b=d+c
@@ -47,13 +49,13 @@ sgt      $t2, $s1, $s0               # Set on $s0 less than $ta   b>a
 beq      $t1, 0, ElseIf              # Branch on equal 0, means if false, b>=c, then run ElseIf
 beq      $t2, 0, ElseIf              # Branch on equal 0, means if false, a>=b, then run ElseIf
 add      $s3, $s0, $s1               # If b<c or a<b are true, then d = a+b
-j         End3                     # Jump to End funtion 
+j         End3                       # Jump to End funtion 
 ElseIf :
    sgt       $t1, $s1, $s2           # Set on $s2 less than $t1   b>c
    sgt       $t2, $s0, $s2           # Set on $s2 less than $t0   a>c
    beq   $t1, 1, Else3               # Branch on equal 0, means if the statement is false, c>=b, then run Else3
    beq   $t2, 1, Else3               # Branch on equal 0, means if the statement is false, c>=a, then run Else3
-   j         End3                  # Jump to End funtion if "ElseIf" statement is False
+   j         End3                    # Jump to End funtion if "ElseIf" statement is False
 Else3:   
    add       $s3, $s1, $s2           # d=b+c
 End3:
