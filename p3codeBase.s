bc89fab6c755fb16d95222b8f12edc395b079089
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2018-03-07 23:42:54 -0800

    Update p3codeBase.s

--- p3codeBase.s
+++ p3codeBase.s
@@ -21,10 +21,10 @@ addi       $t0, 10             # load value 10 to $t0 for next compare function
 slt      $t1, $s0, $t0       # Set on $s0 less than $t0   a<10
 beq      $t1, 0, Else1       # Branch on equal 0, means if the statement is false, a>=10, then run Else2
 add      $s0, $s0, 1         # If a<10 is true, then a = a +1
-j        ENDIF1              # Jump to End funtion
+j        exit              # Jump to End funtion
 Else1 :
          addi   $s0, $s0, -1         # a = a-1
-ENDIF1 :                             # End if function
+exit :                             # End if function
 
 add      $s3,$s2, $s0                # d = c+a
 add      $s2,$s3, $s0                # c = d+a
@@ -33,8 +33,8 @@ slt      $t1, $s1, $t0               # Set on $s1 less than $t0   b<10
 beq      $t1, 0, Else2               # Branch on equal 0, means if the statement is false, b>=10, then run Else2
 add      $s1, $s1, 1                 # If b<10 is true, then a = a+1
 addi     $s2, $s2, -1                # If b<10 is true, then c = c-1
-j        ENDIF2                      # Jump to End funtion
-Else2 :
+j        exit                      # Jump to End funtion
+exit :
          add    $s2, $s2,  1         # c = c+1
          addi   $s1, $s1, -1         # b = b-1
 ENDIF2 :                             # End if function
@@ -47,14 +47,14 @@ sgt      $t2, $s1, $s0               # Set on $s0 less than $ta   b>a
 beq      $t1, 0, ElseIf              # Branch on equal 0, means if false, b>=c, then run ElseIf
 beq      $t2, 0, ElseIf              # Branch on equal 0, means if false, a>=b, then run ElseIf
 add      $s3, $s0, $s1               # If b<c or a<b are true, then d = a+b
-j         ENDIF3                     # Jump to End funtion 
+j         exit                     # Jump to End funtion 
 ElseIf :
    sgt       $t1, $s1, $s2           # Set on $s2 less than $t1   b>c
    sgt       $t2, $s0, $s2           # Set on $s2 less than $t0   a>c
    beq   $t1, 1, Else3               # Branch on equal 0, means if the statement is false, c>=b, then run Else3
    beq   $t2, 1, Else3               # Branch on equal 0, means if the statement is false, c>=a, then run Else3
-   j         ENDIF3                  # Jump to End funtion if "ElseIf" statement is False
-Else3:   
+   j         exit                  # Jump to End funtion if "ElseIf" statement is False
+exit:   
    add       $s3, $s1, $s2           # d=b+c
 ENDIF3 :
 
